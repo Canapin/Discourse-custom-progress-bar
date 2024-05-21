2a3e94e3966d21681d170d1c9d546459f3b5af2e
Coin-coin le Canapin <canapin@gmail.com>
2024-05-22 00:40:38 +0200

    Updated to glimmer and various fixes

--- javascripts/discourse/api-initializers/custom-progress-bar.js
+++ javascripts/discourse/api-initializers/custom-progress-bar.js
@@ -1,23 +1,14 @@
 import { apiInitializer } from "discourse/lib/api";
-import DiscourseTemplateMap from "discourse-common/lib/discourse-template-map";
+import { withPluginApi } from "discourse/lib/plugin-api";
+import CustomProgressBar from "../components/custom-progress-bar";
 
-// Should be the same name as the file in /templates.
-const TEMPLATE_NAME = "custom-progress-bar";
-
-export default apiInitializer("0.11.1", (api, t) => {
+export default apiInitializer("0.11.1", (api) => {
   if (!settings.outlet_name.length) {
     return;
   }
 
-  const { themeTemplates } = DiscourseTemplateMap;
-
-  if (!themeTemplates.has(TEMPLATE_NAME)) {
-    return;
-  }
-
-  // Insert dynamically a new connector based on the template.
-  themeTemplates.set(
-    `connectors/${settings.outlet_name}/${TEMPLATE_NAME}`,
-    themeTemplates.get(TEMPLATE_NAME)
-  );
+  withPluginApi("1.14.0", (api) => {
+    console.log("test");
+    api.renderInOutlet(settings.outlet_name, CustomProgressBar);
+  });
 });
